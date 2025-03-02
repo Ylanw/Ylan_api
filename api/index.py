005f3a6f7fc5315e079937d682690357cca470c1
YLan <3572066061@qq.com>
2025-03-02 11:52:10 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,6 @@ def list_split(items, n):
 def getdata(name):
 
     # 2024-03-29 定义 headers 请求头
-    # 请见 https://github.com/yuhengwei2001/python_github_calendar_api/commit/0f37cfc003f09e99a1892602d8bc2b38137899d2#diff-b014e93fcab9bae29f453d7a616da5eac2f02947f32d02a1a1bf200eeaab5a39L11
     headers = {
         'Referer': 'https://github.com/'+ name,
         'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
@@ -22,11 +21,10 @@ def getdata(name):
         'X-Requested-With': 'XMLHttpRequest'
     }
     # 发送请求时添加 headers 请求头
-    # gitpage = requests.get("https://github.com/" + name)
     gitpage = requests.get("https://github.com/" + name  + "?action=show&controller=profiles&tab=contributions&user_id="+ name, headers=headers)
     data = gitpage.text
     
-    # 2023-11-22 更新正则 https://github.com/Zfour/python_github_calendar_api/issues/18
+    # 2023-11-22 更新正则
     datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
     datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
     
@@ -56,12 +54,12 @@ def getdata(name):
     return returndata
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # 2024-03-15 规范接口的传参方式 https://github.com/Zfour/python_github_calendar_api/issues/20#issuecomment-1999115747
+        # 2024-03-15 规范接口的传参方式
         path = self.path
-        spl=path.split('?')[1:]
-        for kv in spl:
-            key,user=kv.split("=")
-            if key=="user": break
+        if '?' in path:
+            user = path.split('?')[1]
+        else:
+            user = ''
         data = getdata(user)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
